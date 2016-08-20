71dda105e76c59f948c3972ac582438b666d3f1c
TetinhaBR <guicordeiro@gmail.com>
2016-08-20 17:59:30 -0300

    Aula_02

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php 
 	$Hello_Word = "Hello Word";
- ?>
+	define("CONST_HELLO", $Hello_Word);
 
+	$soma = 10+2;
+	$mod = 10% 2;
+ ?>
 <!DOCTYPE html>
 <html>
 	<?php 
-		include "head.php";
-		include "body.php"
+		require_once "head.php";
+		require_once "body.php";
 	?>
 </html>
